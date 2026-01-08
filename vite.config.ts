3badadbf666597fda9dbad85b75c5383ca032536
farhanakhan05 <farhana19feb2004@gmail.com>
2026-01-08 15:11:39 +0530

    Deploy build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BUSIFY-College-Bus-Portal/',
   plugins: [react()],
+  base: '/BUSIFY-College-Bus-Portal/',
+  
 })
 
 
