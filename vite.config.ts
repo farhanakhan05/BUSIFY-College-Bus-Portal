198d04ac7c686f9459fbc53a73f45b0490d0b165
farhanakhan05 <farhana19feb2004@gmail.com>
2026-01-08 20:14:58 +0530

    Build and deploy to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/BUSIFY-College-Bus-Portal/',
-  
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
 
 
